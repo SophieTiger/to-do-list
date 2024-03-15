8edaad210a0ee59a92af74344f4b2b069be01577
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-03-15 08:28:58 +0000

    Add main funtions to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// Add to-dos to the list 
+function addToDo () {
+
+}
+
+// Remove to-dos from the list
+function removeToDo () {
+
+}
+
+// Handles the checked to-dos on the list
+function checkedToDo () {
+
+}
+
+/**
+ * Increments the number of to-dos by 1 
+ * on the "score board"
+ */
+function incrementToDos () {
+
+}
+
+/**
+ * Decrements the number of to-dos by 1 on the
+ * "score board" if the user deletes a task
+ * from the list
+ */
+function decrementToDos () {
+
+}
+
+/**
+ * Increments the number of completed to-dos by
+ * 1 on the "score board"
+ */
+function incrementCompletedToDos () {
+
+}
+
+// Local storage of data
+function saveData() {
+
+}
