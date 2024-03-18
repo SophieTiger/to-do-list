b3cbb841a0c9d7ff124af7900c60472e37b406ee
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-03-18 08:50:37 +0000

    Style list items with checked and unchecked images and create function to add tasks to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-// Add to-dos to the list 
-function addToDo () {
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+// Add task to the list 
+function addTask () {
+    if (inputBox.value === '') {
+        alert("You need to write something!")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+
 
 }
 
